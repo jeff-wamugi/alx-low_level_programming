acc6aeae7082b29869920a816cd2bddc8fefc1b1
Tumuti Wamugi <jefftumuti@gmail.com>
2022-09-08 07:44:48 -0700

    Task4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,20 @@
+/*
+ * File: 4-puts.c
+ * Auth: Jefferson Tumuti 
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Prints "Programming is like building a multilingual
+ *                puzzle, followed by a new line.
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle\n");
+
+	return (0);
+}
