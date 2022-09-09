8df6c58aa42feacb285c423367f331bc28d38e55
Tumuti Wamugi <jefftumuti@gmail.com>
2022-09-09 08:37:31 -0700

    Task1R

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -20,7 +20,7 @@ if (last > 5)
 {
 printf("Last digit of %d is %d and is greater than 5\n", n, last);
 }
-else if (last < 6 && last == 0)
+else if (last < 6 )
 {
 printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last);
 }
