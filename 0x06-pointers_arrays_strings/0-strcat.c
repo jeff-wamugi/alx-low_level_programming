90bba696290c7376c8ff8cb538223d71a44487b3
Tumuti Wamugi <jefftumuti@gmail.com>
2022-09-22 01:29:29 -0700

    Task 0 Resubmission

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _strcat - Concatenates the string pointed to by @src, including the terminating
+ * _strcat - Concatenates string pointed to by @src and the terminating
  *          null byte, to the end of the string pointed to by @dest.
  * @dest: A pointer to the string to be concatenated upon.
  * @src: The source string to be appended to @dest.
