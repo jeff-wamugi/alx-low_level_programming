1e2af34cf4f5e6e4f75f2f26e9696413cc9c62f7
Tumuti Wamugi <jefftumuti@gmail.com>
2022-09-22 01:45:01 -0700

    main.h Recommit

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -5,7 +5,7 @@
  * File: main.h
  * Auth: Jefferson Tumuti
  * Desc: Header file containing prototypes for all functions
- *       used in the 0x05-pointers_arrays_strings directory.
+ *       used in the 0x06-pointers_arrays_strings directory.
  */
 
 char *_strcat(char *dest, char *src);
