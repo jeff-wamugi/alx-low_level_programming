e4a35157b1d50eac4a6e959f4313326e623519be
Tumuti Wamugi <jefftumuti@gmail.com>
2022-09-22 02:59:26 -0700

    main.h

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,13 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
-#include <stdio.h>
 /**
  * File: main.h
  * Auth: Jefferson Tumuti
  * Desc: Header file containing prototypes for all functions
  *       used in the 0x06-pointers_arrays_strings directory.
  */
-
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
